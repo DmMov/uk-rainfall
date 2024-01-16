d90faf7669a20f8504243a243efb0baaa86a6b0c
Dmytro Movchaniuk <dmmov.live@gmail.com>
2024-01-16 17:02:37 +0200

    Created the project

---
+++ Controllers/RainfallController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace UKRainfall.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class RainfallController : ControllerBase
+{
+    [HttpGet]
+    public void Get()
+    {
+           
+    }
+}
